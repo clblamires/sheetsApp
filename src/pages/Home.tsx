62ad68549995227f370e4e32a18cb9d1c1df79aa
Casey Blamires <clblamires@lcmail.lcsc.edu>
2024-02-16 10:25:28 -0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,37 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+// import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
+import React, {useEffect, useState} from 'react';
+
 const Home: React.FC = () => {
+
+  const [data, setData] = useState<any>(null);
+
+  /* 
+  
+  Here's the Sheets API Endpoint
+
+
+  https://sheets.googleapis.com/v4/spreadsheets/1KCM33GCY8BwQSOQL4YNu3TSDMh7e4NDuggZUCoP9_MY/values/Sheet1!A2:D10?key=API_KEY_HERE
+  */
+
+  useEffect( () => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://sheets.googleapis.com/v4/spreadsheets/1KCM33GCY8BwQSOQL4YNu3TSDMh7e4NDuggZUCoP9_MY/values/Sheet1!A1:E10?key=API_KEY_HERE");
+        if( !response.ok) {
+          throw new Error ("Failed to fetch data");
+        }
+        const jsonData = await response.json();
+        setData( jsonData.values );
+      } catch (err) {
+        console.error("Error fetching data: ", err);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +45,19 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        {data && (
+          <ul>
+            {data.map((row: any, rowIndex: number) => (
+              <li key={rowIndex}>
+                <ul>
+                  {row.map((cell: any, cellIndex: number) => (
+                    <li key={cellIndex}>{cell}</li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
+        )}
       </IonContent>
     </IonPage>
   );
